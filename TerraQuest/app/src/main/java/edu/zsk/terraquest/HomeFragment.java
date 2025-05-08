db87113a31783ff5f04ff593a0fbee5d75d8fbf9
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-05-08 06:20:50 +0200

    new change add files1

---
+++ TerraQuest/app/src/main/java/edu/zsk/terraquest/HomeFragment.java
@@ -0,0 +1,13 @@
+package edu.zsk.terraquest;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- TerraQuest/app/src/main/java/edu/zsk/terraquest/LoginFragment.java
+++ TerraQuest/app/src/main/java/edu/zsk/terraquest/LoginFragment.java
@@ -1,12 +1,11 @@
 package edu.zsk.terraquest;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
