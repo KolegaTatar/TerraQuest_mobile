103238090151e8d9a547baaeaa21595555d09f3f
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-05-12 01:36:10 +0200

    add new fragments

--- TerraQuest/app/src/main/java/edu/zsk/terraquest/LoginFragment.java
+++ TerraQuest/app/src/main/java/edu/zsk/terraquest/LoginFragment.java
@@ -13,6 +13,6 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_user_menu, container, false);
     }
 }
