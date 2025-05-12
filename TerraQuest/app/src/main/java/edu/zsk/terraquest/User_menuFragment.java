5ee2b09c3e59458591975056710c6c578f7949de
BergFilip <bergusf2006@gmail.com>
2025-05-12 17:40:38 +0200

    PP and About ready

--- TerraQuest/app/src/main/java/edu/zsk/terraquest/User_menuFragment.java
+++ TerraQuest/app/src/main/java/edu/zsk/terraquest/User_menuFragment.java
@@ -24,6 +24,8 @@ public class User_menuFragment extends Fragment {
 
 
         view.findViewById(R.id.btn_weather).setOnClickListener(v -> navigateTo(new WeatherFragment()));
+        view.findViewById(R.id.btn_privacy).setOnClickListener(v -> navigateTo(new PPFragment()));
+        view.findViewById(R.id.btn_about).setOnClickListener(v -> navigateTo(new AboutFragment()));
 
     }
 
