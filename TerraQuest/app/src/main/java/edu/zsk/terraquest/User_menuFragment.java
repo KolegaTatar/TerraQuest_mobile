390ed53a69f6e099ad6cb3b927f43e3fc772e8a3
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-05-12 03:43:45 +0200

    add user main

--- TerraQuest/app/src/main/java/edu/zsk/terraquest/User_menuFragment.java
+++ TerraQuest/app/src/main/java/edu/zsk/terraquest/User_menuFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class User_menuFragment extends Fragment {
@@ -14,5 +16,21 @@ public class User_menuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_user_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        view.findViewById(R.id.btn_weather).setOnClickListener(v -> navigateTo(new WeatherFragment()));
+
+    }
+
+    private void navigateTo(Fragment fragment) {
+        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
